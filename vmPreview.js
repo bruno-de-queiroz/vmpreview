d4ed0fc04c43c786564b7eb268a1cb7e7f0b5f58
Bruno de Queiroz <cad_bsilva@uolinc.com>
2013-03-22 18:04:01 -0300

    Adjustments

--- vmPreview.js
+++ vmPreview.js
@@ -16,12 +16,13 @@ function i(msg){
 
 // VAR
 var
-		args = ["./jaiminho/422/pagseguro/m16_deposit_overpaid.vm","gerado.html","./jaiminho"]//process.argv.slice(2) // Argumentos
+		args = ["/home/bruno/workspace/jaiminho/422/pagseguro/m16_deposit_overpaid.vm","gerado.html","/home/bruno/workspace/jaiminho"]//process.argv.slice(2) // Argumentos
 	,	baseFile = args[0] // Argumento: Caminho do arquivo base
 	,	generateFile = args[1] // Argumento: Caminho do arquivo a ser gerado
 	,	encode = "utf-8" // Encode
 	,	regLinha = /[^\r\n]+/g // Strip das linhas
 	,	regParse = /#parse\(\"(.*)\"/ // Strip dos comandos #parse
+	, 	regVariables = /\$\!?([\w-_]+|\{[\w]+\})/g
 	,	myRoot = args[2] // Raiz a partir da qual o Velocity considera o ponto de partida
 	,	file = []
 	,	fileCount = 1
@@ -34,95 +35,55 @@ var fileAnalysis = (function(){
 	var c = 0;
 
 	var genFilePath = function(path){
-		path = path.replace(/\s/g,"");
-		path = path.replace("#parse(\"","");
-		path = path.replace("\")","");
-		path = [myRoot,path].join("");
-
-		i(path);
+		var url = path.match(regParse)[1];
+		path = [myRoot,url].join("");
 
 		return path;
 	}
 
-	var isFilePath = function(path){
-		return !!~path.search(regParse);
+	var readFile = function(path,callback) {
+		var file = "";
+
+		i("Processing: "+ path);
+		fs.readFileSync(path).toString().split("\n").forEach(function(line) {
+			if(isFilePath(line)){
+				var url = genFilePath(line);
+				file += readFile(url);
+			} else {
+				file += line + "\n";
+			}
+		});
+
+		if(callback)
+			callback(file);
+
+		return file;
+
 	}
 
-	var openFile = function(file){
-
-		c++;
-
-		var
-			code = ""
-			,qtLines = 0
-		;
-
-		fs.readFile(file.path,encode, function(err,data){
-			if (err) {
-		    	return i(err);
-			}
-
-			code = data.match(regLinha);
-			qtLines = code.length;
-
-
-			for(var line = 0; line < qtLines; line++){
-				if(isFilePath(code[line])){					
-					code[line] = genFilePath(code[line]);
-					fileList.add(code[line],file.index,line);					
-				}
-			}
-
-			file.code = code;			
-
-			var b = fileList.getNext();
-			i(b);
-
-			if(fileList.getTotal()<=c){ // Remover o 13
-				openFile(b);
-			}else{				
-				i(fileAnalysis.generate());
-			}
-			
+	var writeFile = function(fileContent){
+		var file = "./"+generateFile;
+		fs.writeFile(file, fileContent , function(err) {
+			if(err)
+				return i(err)
+			else
+				return i("File copied to: " + file);
 		});
 	}
 
-	var generate = function(){
-		var
-			finalCode = null
-			,totalFiles = fileList.getTotal() - 1
-			,father = 0
-			,fatherLine = 0
-
-		;
-
-		for(var fileToOpen = totalFiles; fileToOpen>=0; fileToOpen--){
-
-			father = fileList.file[fileToOpen].father;
-			fatherLine = fileList.file[fileToOpen].fatherLine;
-			
-
-			//console.log(fileToOpen,fileList.file[fileToOpen].code);
-			i(father);
-			fileList.file[father].code[fatherLine] = fileList.file[fileToOpen].code;
-		}
-
-		finalCode = fileList.file[0].code;
-
-		return finalCode;
-
+	var isFilePath = function(path){
+		return regParse.test(path);
 	}
 
-	var start = function(file){		
-		fileList.add(file, null, null);		
-		var fileToOpen = fileList.getNext();		
-		fileAnalysis.openFile(fileToOpen);
+	var start = function(file){
+		fileList.add(file, null, null);
+		var fileToOpen = fileList.getNext();
+		readFile(fileToOpen.path,writeFile);
 	}
 
 	return{
-		openFile: openFile
-		,generate: generate
-		,start: start
+		readFile: readFile
+		, start: start
 	}
 
 })();
@@ -138,15 +99,15 @@ var fileList = (function(){
 	;
 
 	var add = function(path, father, fatherLine){
-		file.push({index:total, path: path, code: null , father:father, fatherLine:fatherLine});		
+		file.push({index:total, path: path, code: null , father:father, fatherLine:fatherLine});
 		total++;
 	}
 
-	var getNext = function(){		
+	var getNext = function(){
 		list = list + 1;
-		var obj = file[list];						
+		var obj = file[list];
 		return obj;
-	}		
+	}
 
 	var getTotal = function(){
 		return total;
@@ -158,10 +119,10 @@ var fileList = (function(){
 
 	return{
 		file: file
-		,add: add		
+		,add: add
 		,getList:getList
-		,getNext: getNext			
-		,getTotal: getTotal		
+		,getNext: getNext
+		,getTotal: getTotal
 	}
 })();
 
